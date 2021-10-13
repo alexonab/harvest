891f6660c77542db2f7f94a203ebae2da01eb478
Mike M <mikeones42@gmail.com>
2021-10-13 18:16:33 -0400

    Update alpaca.py

--- harvest/api/alpaca.py
+++ harvest/api/alpaca.py
@@ -189,7 +189,7 @@ class Alpaca(StreamAPI):
 
     @API._exception_handler
     def fetch_order_queue(self):
-        return [pos.__dict__["_raw"] for pos in self.api.list_positions()]
+        return [pos.__dict__["_raw"] for pos in self.api.list_orders()]
 
     # --------------- Methods for Trading --------------- #
 
